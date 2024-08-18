ab9f87b90e082fcc0dc8f767b7c6c5a814728a03
AlbertoAlejandro <alberto196g@gmail.com>
2024-08-18 18:40:24 -0400

    added icon technologies to about view

---
+++ storyblok/AppTechnologies.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  blok: {
+    type: Object,
+    required: true,
+    default: () => ({}),
+  },
+})
+</script>
+
+<template>
+  <div v-editable="blok">
+    <div class="flex flex-col gap-5">
+      <h3
+        class="text-base font-theme_bold md:text-xl lg:text-2xl text-head_text"
+      >
+        {{ blok.title }}
+      </h3>
+      <img :src="blok.technologies.cached_url" alt="skillicons" />
+    </div>
+  </div>
+</template>
